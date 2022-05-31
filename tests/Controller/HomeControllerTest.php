3bc853db0023dbe01a3d6a37b21941a1a19f4d7a
maxime <max45270@gmail.com>
2022-05-31 10:04:59 +0200

    add test h1

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+    public function testh1(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/home');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello !');
+    }
+}
